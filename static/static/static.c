a48f73920939d3a68c6710ed80a40f79c47def27
张兆华 <593245631@qq.com>
2018-10-07 09:37:51 +0800

    更改

--- static/static/static.c
+++ static/static/static.c
@@ -1,51 +1,51 @@
-////1 变量
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//void test()
-//{
-//	int i = 0;
-//	i++;
-//	printf("%d\n",i);
-//
-//}
-//int main()
-//{
-//	int i = 0;
-//
-//	for(i=0; i<10; i++)
-//	{
-//		test();	// 1 1 1 1 1 1 1 1 1 1
-//	}
-//	
-//	return 0;
-//}
-//// 2
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//void test()
-//{
-//	static int i = 0;
-//	i++;
-//	printf("%d\n",i);
-//
-//}
-//int main()
-//{
-//	int i = 0;
-//
-//	for(i=0; i<10; i++)
-//	{
-//		test();// 1 2 3 4 5 6 7 8 9 10
-//	}
-//	
-//	return 0;
-//}
-////3 函数
-//int main()
-//{
-//	printf("%d\n",add1(1, 2));
-//	return 0;
-//}
+//1 变量
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void test()
+{
+	int i = 0;
+	i++;
+	printf("%d\n",i);
+
+}
+int main()
+{
+	int i = 0;
+
+	for(i=0; i<10; i++)
+	{
+		test();	// 1 1 1 1 1 1 1 1 1 1
+	}
+	
+	return 0;
+}
+// 2
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void test()
+{
+	static int i = 0;
+	i++;
+	printf("%d\n",i);
+
+}
+int main()
+{
+	int i = 0;
+
+	for(i=0; i<10; i++)
+	{
+		test();// 1 2 3 4 5 6 7 8 9 10
+	}
+	
+	return 0;
+}
+//3 函数
+int main()
+{
+	printf("%d\n",add1(1, 2));
+	return 0;
+}
 //4
 int main()
 {
