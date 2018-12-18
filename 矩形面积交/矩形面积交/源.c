1be19ae79156710d58fd05bff2606eb439ec510d
张兆华 <593245631@qq.com>
2018-12-18 21:52:02 +0800

    修复bug，没有交集的时候面积不为零

--- 矩形面积交/矩形面积交/源.c
+++ 矩形面积交/矩形面积交/源.c
@@ -3,7 +3,7 @@
 #include<Windows.h>
 float S_rectangle(float* a, float* b)
 {
-	float c[4];
+	float c[4];//交的矩形
 	int i = 0;
 	for (i = 0; i < 2; i++)
 	{
@@ -13,18 +13,22 @@ float S_rectangle(float* a, float* b)
 	{
 		c[i] = a[i] <= b[i] ? a[i] : b[i];
 	}
-	return (c[2] - c[0])*(c[3] - c[0]);
+	//for (i = 0; i < 4; i++)
+	//	printf("%0.2f ",c[i]);
+	if (c[2] < c[0] || c[3] < c[1])
+		return 0;
+	return (c[2] - c[0])*(c[3] - c[1]);
 }
 int main()
 {
-	float a[4], b[4];
+	float a[4], b[4];//两个矩形的相对顶点坐标
 	int i = 0;
 	for (i = 0; i < 4; i++)
 		scanf("%f", &a[i]);
 	for (i = 0; i < 4; i++)
 		scanf("%f", &b[i]);
 
-	printf("%0.2f",S_rectangle(a,b));
+	printf("%0.2f\n",S_rectangle(a,b));
 	system("pause");
 	return 0;
 }
