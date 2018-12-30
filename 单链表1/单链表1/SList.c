bcdf62866c98e2d4d588e1c5ee3b4891b396d6e1
张兆华 <593245631@qq.com>
2018-12-30 16:30:50 +0800

    111

--- 单链表1/单链表1/SList.c
+++ 单链表1/单链表1/SList.c
@@ -2,5 +2,49 @@
 
 void SListInit(SList* plist)
 {
+	assert(plist);
+	plist->head = NULL;
+}
+void SListDestory(SList* plist)
+{
+	assert(plist);
+	
+	SListNode* cur = plist->head;
+	SListNode* next;
 
+	while (cur){
+		next = cur->next;
+		free(cur);
+		cur = next;
+	}
+	plist->head = NULL;
+}
+SListNode* BuySListNode(SLTDataType x)
+{
+	SListNode* node = (SListNode*)malloc(sizeof(SListNode));
+
+	assert(node);
+
+	node->_data = x;
+	node->next = NULL;
+
+	return node;
+}
+//Í·²å
+void SListPushFront(SList* plist, SLTDataType x)
+{
+	assert(plist);
+
+	SListNode* node = BuySListNode(x);
+	node->next = plist->head;
+	plist->head = node;
+}
+//Í·É¾
+void SListPopFront(SList* plist)
+{
+	assert(plist);
+
+	SListNode* node = plist->head;
+	plist->head = plist->head->next;
+	free(node);
 }
