46201e2d65e727074d305db250e76117c3a94a6e
张兆华 <593245631@qq.com>
2019-05-29 14:05:37 +0800

    复习结构体大小计算

---
+++ 结构体大小/源.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+//Á·Ï°3
+struct S3
+{
+	double d;
+	char c;
+	int i;
+};
+
+struct S4
+{
+	char c1;
+	struct S3 s3;
+	double d;
+};
+
+struct A
+{
+	int _a : 2; // 8
+	int _b : 5; // 20
+	int _c : 10;// 40
+	//int _d : 30;// 120
+};
+struct S
+{
+	char a : 3;
+	char b : 4;
+	char c : 5;
+	char d : 4;
+};
+
+union MyUnion
+{
+	char a;
+	int b;
+};
+int main()
+{
+	//printf("%d\n", sizeof(struct S3));
+	//printf("%d\n", sizeof(struct S4));
+	//printf("%d\n", sizeof(struct A));
+
+	//struct S s = { 0 };
+	//s.a = 10;
+	//s.b = 12;
+	//s.c = 3;
+	//s.d = 4;
+
+	//printf("%d\n", s.a);
+
+	union MyUnion u;
+	u.b = 0xffffff01;
+
+	printf("%d\n", u.a);
+	return 0;
+}
